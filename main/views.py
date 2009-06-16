5bbad5428715306447369a36612a1df8325ffa67
Stefan Buller <stefan.buller@gmail.com>
2009-06-16 11:13:48 -0500

    Modified the view to utilize the database for enemy/message data. It is no longer hardcoded.

--- main/views.py
+++ main/views.py
@@ -3,66 +3,27 @@
 from django.template import Context, loader
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-import random
 
-ref_strings = {
-	'combat_status': [
-		["You are fighting {en[name]}, the {en[type]}",[]],
-		["{en[name]} is standing here, looking menacing.",[]]
-	],
-	'hit': [
-		["You swing your weapon. It connects!",[]],
-		#{'message': "",
-		#'substitutions':[]
-		#}
-		["You shoot {en[em]} for some damage.",[]]
-	],
-	'miss': [
-		["Your swing goes wide.",[]],
-		["You miss {en[em]} by a mile.",[]]
-	],
-	'enemy_hit': [
-		["You are wounded by {en[eir]} vicious strike.",[]],
-		["{en[Ey]} {words[0]} you.",[["hits","hit"]]]
-	],
-	'enemy_miss': [
-		["{en[Ey]} {words[0]} to hit you, but {words[1]}. {en[Ey]} {words[2]} into the corner to cry.",[["tries","try"],["misses","miss"],["goes","go"]]],
-		["You deftly dodge {en[eir]} {words[0]}.",[["attack","attacks"]]]
-	],
-}
+from models import CombatMessage, Enemy
 
-#pronouns = {'em': ["him", "her", "it", "them"],
-	#'ey': ["he", "she", "it", "they"].
-	#'eir': ["his", "her", "its", "their"].
-	#'emself': ["himself", "herself", "itself", "themselves"],
-	#}
-
-enemy = {'type': ["Giant", "Undead", "Kobold"],
-	'name': "Billy-Joe",
-	'em': "him",
-	'ey': "he",
-	'eir': "his",
-	'emself': "himself",
-	'Em': 'Him',
-	'Ey': 'He',
-	'Eir': 'His',
-	'Emself': 'Himself',
-	'plurality': 0
-	}
-
+import random, json
 
 def index(request):
 	return render_to_response('main/index.djt',{})
 
 def combat(request):
 	t = loader.get_template('main/combat.djt')
-	whomessage = random.choice(ref_strings['combat_status'])
-	youmessage = random.choice(ref_strings['hit'])
-	enemymessage = random.choice(ref_strings['enemy_miss'])
+	enemy = Enemy.objects.filter(name='Billy-Joe').get()
+	combat_status = CombatMessage.objects.filter(action='who')
+	hit = CombatMessage.objects.filter(action='you hit')
+	enemy_miss = CombatMessage.objects.filter(action='enemy misses')
+	whomessage = random.choice(combat_status)
+	youmessage = random.choice(hit)
+	enemymessage = random.choice(enemy_miss)
 	c = Context({
 			'combat_text': [
-			whomessage[0].format(en=enemy, words=map(lambda x: x[enemy['plurality']], whomessage[1])),
-			youmessage[0].format(en=enemy,words=map(lambda x: x[enemy['plurality']], youmessage[1])),
-			enemymessage[0].format(en=enemy,words=map(lambda x: x[enemy['plurality']],enemymessage[1])),]
+			whomessage.message.format(en=enemy, words=map(lambda x: x[enemy.plurality], json.loads(whomessage.words))),
+			youmessage.message.format(en=enemy,words=map(lambda x: x[enemy.plurality], json.loads(youmessage.words))),
+			enemymessage.message.format(en=enemy,words=map(lambda x: x[enemy.plurality],json.loads(enemymessage.words))),]
 		})
 	return HttpResponse(t.render(c))
