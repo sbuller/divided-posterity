58fc1832a5dd64ee6f067ab50c83d49a1d281f2b
Stefan Buller <stefan.buller@gmail.com>
2009-06-24 14:32:14 -0500

    Added a bunch of static files to the repository. Most of these come from the old repository.

--- urls.py
+++ urls.py
@@ -26,4 +26,6 @@ urlpatterns = patterns('dp.main.views',
 urlpatterns += patterns('',
 	(r'^login$', 'django.contrib.auth.views.login'),
 	(r'^logout$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
+	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
+
 )
