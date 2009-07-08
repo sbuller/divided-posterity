b7148cc8020114f7dfff4674ad39402a1a5388fd
Stefan Buller <stefan.buller@gmail.com>
2009-07-08 11:16:46 -0500

    Skills now have images. The skill repository has been removed from combat.djt, and the skill bar is now filled from the Heros list of skills.

--- main/models/skill.py
+++ main/models/skill.py
@@ -7,6 +7,7 @@ class Skill(models.Model):
 		app_label = 'main'
 	name = models.CharField(max_length=50)
 	action = models.ForeignKey(Action,null=True)
+	image_url = models.URLField()
 
 	def __unicode__(self):
 		return self.name
