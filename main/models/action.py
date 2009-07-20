b0f9cd2a1cc0c698bb892c6336f957750e565080
Stefan Buller <stefan.buller@gmail.com>
2009-07-20 14:41:54 -0500

    Changed the code in generic_attack to use None and float('Inf') instead of False and True as special values. This is safer.

--- main/models/action.py
+++ main/models/action.py
@@ -22,16 +22,18 @@ class Action(models.Model):
 
 	@classmethod
 	def generic_attack(cls, actor, target, stats={}):
-		#returns damage dealt (False on miss)
-		#case accuracy: True, attack hits, False: attack misses, else: roll some dice
-		#case critical: True, critical hit, False: normal damage, else: roll some dice
-		#case exact_damage: False: roll some dice, else: damage = exact_damage
+		'''
+		returns damage dealt (None on miss)
+		case accuracy: 'Inf', attack hits, None: attack misses, else: roll some dice
+		case critical: 'Inf', critical hit, None: normal damage, else: roll some dice
+		case exact_damage: None: roll some dice, else: damage = exact_damage
+		'''
 		import random, math
 		from trigger import Trigger
 		DEFAULT_STATS = {
 			"accuracy": actor.finesse,
 			"critical": actor.lore,
-			"exact_damage": False,
+			"exact_damage": None,
 			"might": actor.brawn,
 			"weapon": 3, #actor.weapon_power
 			"plus_damage": 0,
@@ -45,23 +47,23 @@ class Action(models.Model):
 
 		Trigger.invoke_triggers(target, "receive attack")
 		Trigger.invoke_triggers(actor, "deal attack")
-		if stats['accuracy'] != False and (stats['accuracy'] == True or stats['accuracy'] * 2.0 / 3.0 / target.charm >= random.random()):
+		if stats['accuracy'] != None and float(stats['accuracy']) * 2.0 / 3.0 / target.charm >= random.random():
 			Trigger.invoke_triggers(target, "receive hit")
 			Trigger.invoke_triggers(actor, "deal hit")
 		else:
 			Trigger.invoke_triggers(target, "receive miss")
 			Trigger.invoke_triggers(actor, "deal miss")
-			return False
+			return None
 
-		if stats['exact_damage'] == False:
+		if stats['exact_damage'] == None:
 			damage = int(math.ceil(stats['might'] * stats['weapon'] * (0.8 + 0.4 * random.random()) / target.stamina))
 			damage += stats['plus_damage']
 		else:
 			damage = stats['exact_damage']
 
 		if damage > 0:
-			if stats['critical'] != False:
-				if stats['critical'] == True or 0.25 * (stats['critical']-target.lore)/target.lore >= random.random():
+			if stats['critical'] != None:
+				if 0.25 * (float(stats['critical'])-target.lore)/target.lore >= random.random():
 					damage = int(math.ceil(damage * 1.5))
 			Action.damage(target, damage)
 		else:
